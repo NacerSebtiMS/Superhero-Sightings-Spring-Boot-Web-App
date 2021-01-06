b050d4f36f4389efce2980348096d130a5d0f16b
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-06 15:17:18 -0500

    Project init

---
+++ superherosightings/src/main/java/com/sg/superherosightings/SuperherosightingsApplication.java
@@ -0,0 +1,13 @@
+package com.sg.superherosightings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperherosightingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperherosightingsApplication.class, args);
+	}
+
+}
---
+++ superherosightings/src/test/java/com/sg/superherosightings/SuperherosightingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.superherosightings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherosightingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
