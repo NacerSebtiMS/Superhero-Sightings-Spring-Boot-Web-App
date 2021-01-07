83af643332ed7f80a29387d9e93e55ce3a7aef71
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-07 16:47:53 -0500

    Autowired all test files

--- superherosightings/src/test/java/com/sg/superherosightings/dao/SightingDaoDBTest.java
+++ superherosightings/src/test/java/com/sg/superherosightings/dao/SightingDaoDBTest.java
@@ -11,12 +11,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  *
  * @author nacer
  */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class SightingDaoDBTest {
+    
+    @Autowired
+    HeroDao heroDao;
+    
+    @Autowired
+    LocationDao locationDao;
+    
+    @Autowired
+    OrganizationDao organizationDao;
+    
+    @Autowired
+    SightingDao sightingDao;
+    
+    @Autowired
+    SuperpowerDao superpowerDao;
     
     public SightingDaoDBTest() {
     }
