661488fd4429522b33520d908ca63f0e9d2af7c3
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-07 12:12:40 -0500

    Implemented Location DAO DB

--- superherosightings/src/main/java/com/sg/superherosightings/dao/LocationDaoDB.java
+++ superherosightings/src/main/java/com/sg/superherosightings/dao/LocationDaoDB.java
@@ -9,37 +9,76 @@ import com.sg.superherosightings.models.Location;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author nacer
  */
+@Repository
 public class LocationDaoDB implements LocationDao {
+    
+    @Autowired
+    JdbcTemplate jdbc;
 
     @Override
     public Location getLocationById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            final String SELECT_LOCATION_BY_ID = "SELECT * FROM Location WHERE LocationId = ?";
+            return jdbc.queryForObject(SELECT_LOCATION_BY_ID, new LocationMapper(), id);
+        } catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public List<Location> getAllLocations() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String SELECT_ALL_LOCATIONS = "SELECT * FROM Location";
+        return jdbc.query(SELECT_ALL_LOCATIONS, new LocationMapper());
     }
 
     @Override
+    @Transactional
     public Location addLocation(Location location) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String INSERT_LOCATION = "INSERT INTO Location(Name, Latitude, Longitude, Description, AddressInformation) "
+                + "VALUES(?,?,?,?,?)";
+        jdbc.update(INSERT_LOCATION,
+                location.getName(),
+                location.getLatitude(),
+                location.getLongitude(),
+                location.getDescription(),
+                location.getAddressInformation());
+        
+        int newId = jdbc.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        location.setId(newId);
+        return location;
     }
 
     @Override
     public void updateLocation(Location location) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String UPDATE_LOCATION = "UPDATE Location SET Name = ?, Latitude = ?, Longitude = ?, Description = ?, AddressInformation = ?"
+                + "WHERE LocationId = ?";
+        jdbc.update(UPDATE_LOCATION,
+                location.getName(),
+                location.getLatitude(),
+                location.getLongitude(),
+                location.getDescription(),
+                location.getAddressInformation());
     }
 
     @Override
+    @Transactional
     public void deleteLocationById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String DELETE_SIGHTING = "DELETE FROM Sighting WHERE LocationId = ?";
+        jdbc.update(DELETE_SIGHTING, id);
+        
+        final String DELETE_LOCATION = "DELETE FROM Location WHERE LocationId = ?";
+        jdbc.update(DELETE_LOCATION, id);
     }
     
     public static final class LocationMapper implements RowMapper<Location> {
@@ -47,7 +86,7 @@ public class LocationDaoDB implements LocationDao {
         @Override
         public Location mapRow(ResultSet rs, int index) throws SQLException {
             Location location = new Location();
-            location.setId(rs.getInt("id"));
+            location.setId(rs.getInt("LocationId"));
             location.setName(rs.getString("name"));
             location.setLatitude(rs.getDouble("latitude"));
             location.setLongitude(rs.getDouble("longitude"));
