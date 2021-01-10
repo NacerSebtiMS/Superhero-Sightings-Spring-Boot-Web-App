b9cfd09bf375c8ba94a6be4dd792bef4abba990a
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-09 19:11:33 -0500

    Added and setup controllers

---
+++ superherosightings/src/main/java/com/sg/superherosightings/controller/SuperpowerController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.superherosightings.controller;
+
+import com.sg.superherosightings.dao.HeroDao;
+import com.sg.superherosightings.dao.LocationDao;
+import com.sg.superherosightings.dao.OrganizationDao;
+import com.sg.superherosightings.dao.SightingDao;
+import com.sg.superherosightings.dao.SuperpowerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author nacer
+ */
+@Controller
+public class SuperpowerController {
+    
+    @Autowired
+    HeroDao heroDao;
+    
+    @Autowired
+    LocationDao locationDao;
+    
+    @Autowired
+    OrganizationDao organizationDao;
+    
+    @Autowired
+    SightingDao sightingDao;
+    
+    @Autowired
+    SuperpowerDao superpowerDao;
+    
+    
+    
+}
