c206a989bee7eb1fe2244bef5af5bf10b0071f9b
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-10 01:12:38 -0500

    Setup controller and html file of superpowers

--- superherosightings/src/main/java/com/sg/superherosightings/controller/SuperpowerController.java
+++ superherosightings/src/main/java/com/sg/superherosightings/controller/SuperpowerController.java
@@ -7,6 +7,8 @@ package com.sg.superherosightings.controller;
 
 import com.sg.superherosightings.service.SuperpowerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
@@ -20,6 +22,10 @@ public class SuperpowerController {
         this.superpowerService = superpowerService;
     }
     
-    
+    @GetMapping("superpowers")
+    public String displaySuperpowers(Model model) {
+        
+        return "superpowers";
+    }
     
 }
