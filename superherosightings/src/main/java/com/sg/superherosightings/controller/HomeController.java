05163baf824b1c061c1d12702c4d3db69a4ed678
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-09 20:55:56 -0500

    Added home controller and index.html

---
+++ superherosightings/src/main/java/com/sg/superherosightings/controller/HomeController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.superherosightings.controller;
+
+import com.sg.superherosightings.dao.HeroDao;
+import com.sg.superherosightings.dao.LocationDao;
+import com.sg.superherosightings.dao.OrganizationDao;
+import com.sg.superherosightings.dao.SightingDao;
+import com.sg.superherosightings.dao.SuperpowerDao;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author nacer
+ */
+@Controller
+public class HomeController {
+    
+    @Autowired
+    HeroDao heroDao;
+    
+    @Autowired
+    LocationDao locationDao;
+    
+    @Autowired
+    OrganizationDao organizationDao;
+    
+    @Autowired
+    SightingDao sightingDao;
+    
+    @Autowired
+    SuperpowerDao superpowerDao;
+    
+    @GetMapping("/")
+    public String displayIndex(Model model) {
+        
+        model.addAttribute("test", "TEST");
+
+        return "index";
+    }
+    
+}
