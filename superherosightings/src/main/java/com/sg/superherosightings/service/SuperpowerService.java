66c6e25408ba237207b1e76c16d8d165707ecefc
Nacer Sebti <nacersebti.ms@gmail.com>
2021-01-09 23:16:12 -0500

    Bridge functions Superpower

--- superherosightings/src/main/java/com/sg/superherosightings/service/SuperpowerService.java
+++ superherosightings/src/main/java/com/sg/superherosightings/service/SuperpowerService.java
@@ -10,6 +10,8 @@ import com.sg.superherosightings.dao.LocationDao;
 import com.sg.superherosightings.dao.OrganizationDao;
 import com.sg.superherosightings.dao.SightingDao;
 import com.sg.superherosightings.dao.SuperpowerDao;
+import com.sg.superherosightings.models.Superpower;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,4 +36,27 @@ public class SuperpowerService {
     
     @Autowired
     SuperpowerDao superpowerDao;
+    
+    // SERVICE FUNCTION
+    
+    
+    
+    // EXTERN DAO FUNCTIONS  
+
+    // LOCAL DAO FUNCTIONS  
+    public Superpower getSuperpowerById(int id){
+        return superpowerDao.getSuperpowerById(id);      
+    }
+    List<Superpower> getAllSuperpowers(){
+        return superpowerDao.getAllSuperpowers();
+    }
+    Superpower addHero(Superpower superpower){
+        return superpowerDao.addSuperpower(superpower);
+    }
+    void updateSuperpower(Superpower hero){
+        superpowerDao.updateSuperpower(hero);
+    }
+    void deleteSuperpowerById(int id){
+        superpowerDao.deleteSuperpowerById(id);
+    }
 }
